4b18d1232a829dd6d4f5ce7d3fc2deb61f06c9cb
Hakan Erdem Temiz <hakanerdemtemiz@gmail.com>
2019-03-08 12:02:43 +0300

    code refactoring path order more readable
    NODE_PATH=src/

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
-import CommentBox from "../CommentBox";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 it("shows a comment box", () => {
   // NOT needed i use enyzme instead of this part
@@ -16,4 +17,9 @@ it("shows a comment box", () => {
 
   const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
