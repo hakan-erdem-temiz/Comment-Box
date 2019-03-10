fc1f781d13f1d274b92a5288661b77786ac04372
Hakan Erdem Temiz <hakanerdemtemiz@gmail.com>
2019-03-10 23:53:32 +0300

    redux side of application

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = {
+    comment: ""
+  };
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+
+    //call action creater
+    this.props.saveComment(this.state.comment);
+
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(CommentBox);
